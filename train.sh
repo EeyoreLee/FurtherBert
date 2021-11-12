25c2e085ec658df37fabf0a11e2dd5468e0d47d5
lichunyu <lichunyu@videoplsdeMacBook-Pro.local>
2021-11-12 18:30:44 +0800

    change apt to yum

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-apt install -y libopenmpi-dev
+#apt update && apt install -y libopenmpi-dev && apt install -y build-essential
+yum install -y openmpi-devel && export CC=/usr/lib64/openmpi/bin/mpicc
 pip install -r ./code/requirements.txt 
 rm -r ~/.cache/torch_extensions/utils
 
@@ -11,7 +12,8 @@ do
     HIDDEN_SIZE=`expr ${HIDDEN_SIZE_BEGIN} + ${addition} \* 32`
     echo "current hidden size is ${HIDDEN_SIZE} ..."
 
-    CUDA_VISIBLE_DEVICES=0,1 deepspeed --num_gpus=2 ./code/train.py \
+    CXX=g++ deepspeed --num_gpus=2 ./code/train.py \
+    # CUDA_VISIBLE_DEVICES=0,1 deepspeed --num_gpus=2 ./code/train.py \
     --deepspeed ./code/ds_auto_config.json \
     --lr=1e-4 \
     --epochs=10 \
